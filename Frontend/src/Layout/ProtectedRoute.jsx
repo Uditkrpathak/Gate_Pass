dbe2135ca2cac2c91d726f8c693bf96aeac32623
ashishjha950 <ashishjha97099@gmail.com>
2025-10-03 11:38:03 +0530

    route navigation corrected

--- Frontend/src/Layout/ProtectedRoute.jsx
+++ Frontend/src/Layout/ProtectedRoute.jsx
@@ -2,12 +2,13 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ children, allowedRole }) => {
-  const user = localStorage.getItem("user"); 
-  console.log(user)
+  const storedUser = localStorage.getItem("user");
 
-  if (!user) {
+  if (!storedUser) {
     return <Navigate to="/login" replace />;
   }
+
+  const user = JSON.parse(storedUser);
 
   if (user.role !== allowedRole) {
     return <Navigate to="/unauthorized" replace />;
